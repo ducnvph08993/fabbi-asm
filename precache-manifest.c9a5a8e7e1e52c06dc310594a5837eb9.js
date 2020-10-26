872461b89af62f45a074973497b393b728299e4f
ducnvph08993 <57821959+ducnvph08993@users.noreply.github.com>
2020-10-26 21:44:15 +0700

    Updates

---
+++ precache-manifest.c9a5a8e7e1e52c06dc310594a5837eb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0feece40b47542bf39174e77c10ba5",
+    "url": "/fabbi-asm/index.html"
+  },
+  {
+    "revision": "d527b4006d43b34591d5",
+    "url": "/fabbi-asm/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "5a8b8bf8f3cd7dec3d59",
+    "url": "/fabbi-asm/static/css/main.f6dff740.chunk.css"
+  },
+  {
+    "revision": "d527b4006d43b34591d5",
+    "url": "/fabbi-asm/static/js/2.b6ad869f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/fabbi-asm/static/js/2.b6ad869f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a8b8bf8f3cd7dec3d59",
+    "url": "/fabbi-asm/static/js/main.24005a4a.chunk.js"
+  },
+  {
+    "revision": "487f187ccd90e449bf6c",
+    "url": "/fabbi-asm/static/js/runtime-main.8fe7851f.js"
+  }
+]);
